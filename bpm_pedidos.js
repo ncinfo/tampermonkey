4a5a1f09e0c7ba73552142b8b819dffbaf3c3604
Wagner Felipe Leszkiewicz <wagcbjr@gmail.com>
2021-10-25 11:39:38 -0300

    Update bpm_pedidos.js

--- bpm_pedidos.js
+++ bpm_pedidos.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pedidos NC Sistemas (Alterado)
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Pedidos NC Sistemas
 // @author       Wagner Felipe Leszkiewicz
 // @match        https://platform.senior.com.br/ecm-frontend/*
